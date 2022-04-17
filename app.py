5f04b6aeaa48d62253127aee60296fcbf68308c4
John Andrei Cahalhal <74064844+TArabiSA@users.noreply.github.com>
2022-04-18 06:43:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from PIL import Image
 import streamlit as st
 st.set_page_config(page_title="Taba Pares House", page_icon=":chart_with_upwards_trend:", layout= "wide")
 from multiapp import MultiApp
-from apps import week, month, quarter, year
+from apps import week_app, month_app, quarter_app, year_app
 
 
 img_contact_form = Image.open("C:/Users/yakul/mutipage/images/tph.jpg")
@@ -15,9 +15,9 @@ st.markdown("""
 """)
 
 # Add all your application here
-app.add_app("WEEK", week.app)
-app.add_app("MONTH", month.app)
-app.add_app("QUARTER", quarter.app)
-app.add_app("YEAR", year.app)
+app.add_app("WEEK", week_app.app)
+app.add_app("MONTH", month_app.app)
+app.add_app("QUARTER", quarter_app.app)
+app.add_app("YEAR", year_app.app)
 # The main app
 app.run()
