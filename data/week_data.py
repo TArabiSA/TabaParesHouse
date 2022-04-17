d47840dc6aa79a05007d95d141981fd4d34e8b02
John Andrei Cahalhal <74064844+TArabiSA@users.noreply.github.com>
2022-04-18 06:37:12 +0800

    Update week_data.py

--- data/week_data.py
+++ data/week_data.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-dataset = st.container()
 
 
-with dataset:
-    week_data = pd.read_csv("C:/Users/yakul/mutipage/data/week.csv")
-    week_data.tail()
+def create_weektable:
+    create_weektable = pd.read_csv("C:/Users/yakul/mutipage/data/week.csv")
+    create_weektable.tail()
+    return create_weektable
