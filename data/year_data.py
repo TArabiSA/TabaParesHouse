fe5b56d24a8885b8c4455cc74dfb4feb0e0cdf45
John Andrei Cahalhal <74064844+TArabiSA@users.noreply.github.com>
2022-04-18 07:49:16 +0800

    Update year_data.py

--- data/year_data.py
+++ data/year_data.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-dataset = st.container()
 
-
-with dataset:
-    year_data = pd.read_csv('C:/Users/yakul/mutipage/data/year.csv')
-    year_data.tail()
+def create_yeartable():
+    create_yeartable = pd.read_csv("C:/Users/yakul/mutipage/data/week.csv")
+    create_yeartable.tail()
+    return create_yeartable
 
