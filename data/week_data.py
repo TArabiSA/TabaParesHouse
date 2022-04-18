a2db2fb4dfac7cf3efed949314868af8d4d2472e
John Andrei Cahalhal <74064844+TArabiSA@users.noreply.github.com>
2022-04-19 03:59:20 +0800

    Update week_data.py

--- data/week_data.py
+++ data/week_data.py
@@ -6,6 +6,6 @@ from data import week.csv
 
 
 def create_weektable():
-    create_weektable = pd.read_csv("week.csv")
+    create_weektable = pd.read_csv("C:/Users/yakul/mutipage/data/week.csv")
     create_weektable.tail()
     return create_weektable
