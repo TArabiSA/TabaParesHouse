64fa55600c742385ca3f2ee67500584e41c136a5
John Andrei Cahalhal <74064844+TArabiSA@users.noreply.github.com>
2022-04-19 05:39:05 +0800

    Update month_data.py

--- data/month_data.py
+++ data/month_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+
 def create_monthtable():
     create_monthtable = pd.read_csv("C:/Users/yakul/mutipage/data/week.csv")
     create_monthtable.tail()
